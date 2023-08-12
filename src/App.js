d949d2fc0b9d540b7d726f21b5c5151ddad1a595
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2023-08-11 23:25:31 -0400

    getting all repos from current user

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Homepage, First } from './pages';
-import Login from "./components/LogIn"; 
+import { Homepage, First } from "./pages";
+import Login from "./components/LogIn";
+import BackendTesting from "./components/BackendTesting";
 
 function App() {
   return (
-  
- <Router>
+    <Router>
       <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route
+        <Route path="/" element={<Homepage />} />
+        <Route
           path="/login"
           element={<Login name="login" displayName="Log In" />}
         />
-      <Route path="/pg1" element={<First />} />
-     
+        <Route path="/pg1" element={<First />} />
+        <Route path="/backend-testing" element={<BackendTesting />} />
       </Routes>
-      </Router>
+    </Router>
   );
 }
 
